c587e132e35894f55b86d63270edbf026c0c362b
Exem <chernyakov.ghost@mail.ru>
2021-05-30 22:18:55 +0500

    Добавьте файлы проекта.

---
+++ TRPO2/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿
+namespace TRPO2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.index = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.value = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.StartBTN = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.index,
+            this.value});
+            this.dataGridView1.Location = new System.Drawing.Point(613, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(158, 266);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // index
+            // 
+            this.index.HeaderText = "Индекс";
+            this.index.Name = "index";
+            this.index.Width = 50;
+            // 
+            // value
+            // 
+            this.value.HeaderText = "Значение";
+            this.value.Name = "value";
+            this.value.Width = 70;
+            // 
+            // StartBTN
+            // 
+            this.StartBTN.Location = new System.Drawing.Point(613, 284);
+            this.StartBTN.Name = "StartBTN";
+            this.StartBTN.Size = new System.Drawing.Size(81, 43);
+            this.StartBTN.TabIndex = 2;
+            this.StartBTN.Text = "Начать";
+            this.StartBTN.UseVisualStyleBackColor = true;
+            this.StartBTN.Click += new System.EventHandler(this.StartBTN_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(700, 289);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Дисперсия";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(700, 309);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Средн.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(868, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Дисперсия";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(868, 126);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Дисперсия";
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(595, 300);
+            this.chart1.TabIndex = 7;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1148, 339);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.StartBTN);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button StartBTN;
+        private System.Windows.Forms.DataGridViewTextBoxColumn index;
+        private System.Windows.Forms.DataGridViewTextBoxColumn value;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ TRPO2/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace TRPO2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void StartBTN_Click(object sender, EventArgs e) //кнопка начать
+        {
+            vGenerate();
+            vMakeDispersion();
+            vMakeIntervals();
+        }
+
+        private double fGenerateNumber(int x) //сгенерированное случайное число прозодит через 
+                                              //формулу экспоненциального закона распределения 
+        {
+            double c; 
+            c = 3 * Math.Exp((3 * -1) * x);
+            return c; 
+        }
+
+        private List<double> values = new List<double>();
+
+        private void vMakeIntervals()
+        {
+            /*
+            int amount, counter = 0, j = 0;
+            double interval, min = 100, max = -100;
+            double[] array = new double[100];
+
+            List<int> counters = new List<int>();
+            values.Sort();
+            min = values[0]; max = values[values.Count - 1];
+
+            label3.Text = min.ToString(); label4.Text = max.ToString();
+
+            amount = 10;
+            interval = (max - min) / amount;
+
+            double left = min, right = min + interval; //определение левой и правой границы интервала
+          
+            for (int i = 0; i < amount; i++)
+            {
+                foreach(double value in values)
+                {
+                    if (value >= left && value <= right) { counter++;  }
+                }
+                left += interval; right += interval;
+                counters.Add(counter);
+                
+                counter = 0;
+            }
+           
+            
+            chart1.Series.Add(new Series("0"));
+            chart1.Series["0"].Points.DataBindY(counters);*/
+            chart1.Series.Clear();
+            List<int> Counters = new List<int>();
+            double Min = values[0], Max = values[values.Count - 1];
+            double Interval = (Max - Min) / 10;
+            double Left = Min, Right = Left + Interval;
+            int counter = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                foreach (double j in values)
+                {
+                    if (j >= Left && j <= Right)
+                    {
+                        counter++; label3.Text = Interval.ToString();
+                    }
+                }
+                Left += Interval;
+                Right += Interval;
+                Counters.Add(counter);
+                counter = 0;
+            }
+            chart1.Series.Add(new Series("Exp")); // Создание нового чарта гистограммы и ее заполнение
+            chart1.Series["Exp"].Points.DataBindY(Counters);
+
+        }
+
+        private void vMakeDispersion() //функция подсчета дисперсии
+        {
+            double a, D, sumvalueB = 0, sumvalueA = 0;
+
+            foreach(double value in values)
+            {
+                a = Math.Sqrt(value);
+                sumvalueA += a;
+                sumvalueB += value;
+            }
+
+            double avgA = sumvalueA/99, avgB = Math.Sqrt(sumvalueB/99);
+
+            D = avgA - avgB;
+            label2.Text = avgA.ToString();
+            label1.Text = D.ToString();
+
+        }
+
+        private void vGenerate() //генерация случайного числа
+        {
+            Random rnd = new Random();
+            double value, sumValue = 0; int x;
+            
+            for(int i = 0; i < 100; i++)
+            {
+                /*if(i%10 == 0)
+                {
+                    this.chart1.Series[0].Points.Add(sumValue);
+                    sumValue = 0;
+                }*/
+
+                x = rnd.Next(0, 50);
+                value = fGenerateNumber(x);
+                values.Add(value);
+                sumValue += value;
+                dataGridView1.Rows.Add(i, value);
+            }
+     
+        }
+
+    }
+}
